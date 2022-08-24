db973856bd9ce0df3a29066b60f52859f69a3483
victorinhance <victor-inhance@hotmail.com>
2022-08-24 18:53:59 -0300

    aula-segundodia

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -10,8 +10,4 @@ profissao = "Assistente Administrativo"
 idade = "21"
 lugar = "Inglaterra"
 
-console.log (nome)
-console.log (email)
-console.log (profissao)
-console.log (idade)
-console.log (lugar)
+console.log ("Olá", nome, "! somos aqui da agência de turismo viagem dos sonhos. Estamos te escrevendo esse e-mail, pois acabamos de confirmar as compras das passagens aéreas para", lugar, ". Confirma pra gente alguns dados? A sua idade é", idade, ", você é ", profissao, "e o seu e-mail é", email, "? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
